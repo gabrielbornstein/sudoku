6cfe37f868953e937ea7170cc2144d5f5862c879
David Krinker <David@CasaTunes.com>
2019-05-14 14:02:34 -0400

    * Program.cs: Debugged initial implementation.
    * Game.cs:
    * UnitTest1.cs:

--- testSuite/UnitTest1.cs
+++ testSuite/UnitTest1.cs
@@ -28,9 +28,28 @@ namespace Tests
 
             Game currGame = new Game(board1);
             GridValueEnum value = currGame.GetGridValue(6, 8);
-            System.Console.WriteLine(board1[6, 8]);
-            System.Console.WriteLine(value);
-            //Assert.AreEqual(value, GridValueEnum.Digit_4);
+            Assert.AreEqual(value, GridValueEnum.Digit_3);
+        }
+
+        [Test]
+        public void Test2()
+        {
+            GridValueEnum[,] board1 = new GridValueEnum[,]
+            {
+                { GridValueEnum.Digit_5, GridValueEnum.Digit_3, GridValueEnum.Blank, GridValueEnum.Blank, GridValueEnum.Digit_7, GridValueEnum.Blank, GridValueEnum.Blank, GridValueEnum.Blank, GridValueEnum.Blank },
+                { GridValueEnum.Digit_6, GridValueEnum.Blank, GridValueEnum.Blank, GridValueEnum.Digit_1, GridValueEnum.Digit_9, GridValueEnum.Digit_5, GridValueEnum.Blank, GridValueEnum.Blank, GridValueEnum.Blank },
+                { GridValueEnum.Blank, GridValueEnum.Digit_9, GridValueEnum.Digit_8, GridValueEnum.Blank, GridValueEnum.Blank, GridValueEnum.Blank, GridValueEnum.Blank, GridValueEnum.Digit_6, GridValueEnum.Blank },
+                { GridValueEnum.Digit_8, GridValueEnum.Blank, GridValueEnum.Blank, GridValueEnum.Blank, GridValueEnum.Digit_6, GridValueEnum.Blank, GridValueEnum.Blank, GridValueEnum.Blank, GridValueEnum.Digit_3 },
+                { GridValueEnum.Digit_4, GridValueEnum.Blank, GridValueEnum.Blank, GridValueEnum.Digit_8, GridValueEnum.Blank, GridValueEnum.Digit_3, GridValueEnum.Blank, GridValueEnum.Blank, GridValueEnum.Digit_1 },
+                { GridValueEnum.Digit_7, GridValueEnum.Blank, GridValueEnum.Blank, GridValueEnum.Blank, GridValueEnum.Digit_2, GridValueEnum.Blank, GridValueEnum.Blank, GridValueEnum.Blank, GridValueEnum.Digit_6 },
+                { GridValueEnum.Blank, GridValueEnum.Digit_6, GridValueEnum.Blank, GridValueEnum.Blank, GridValueEnum.Blank, GridValueEnum.Blank, GridValueEnum.Digit_2, GridValueEnum.Digit_8, GridValueEnum.Blank },
+                { GridValueEnum.Blank, GridValueEnum.Blank, GridValueEnum.Blank, GridValueEnum.Digit_4, GridValueEnum.Digit_1, GridValueEnum.Digit_9, GridValueEnum.Blank, GridValueEnum.Blank, GridValueEnum.Digit_5 },
+                { GridValueEnum.Blank, GridValueEnum.Blank, GridValueEnum.Blank, GridValueEnum.Blank, GridValueEnum.Digit_8, GridValueEnum.Digit_6, GridValueEnum.Blank, GridValueEnum.Digit_7, GridValueEnum.Digit_9 },
+            };
+
+            Game currGame = new Game(board1);
+            GridValueEnum value = currGame.GetGridValue(6, 8);
+            Assert.AreEqual(value, GridValueEnum.Digit_4);
         }
     }
 }
